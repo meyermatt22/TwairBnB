298eee034e206d234f2753c779384eae6d436563
Your Name <Your Email>
2023-03-17 12:12:11 -0500

    copy

--- backend/db/migrations/20230316171736-create-user.js
+++ backend/db/migrations/20230316171736-create-user.js
@@ -1,16 +1,13 @@
-'use strict';
+"use strict";
 
-
-
-/* @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -27,32 +24,24 @@ module.exports = {
         allowNull: false,
         unique: true
       },
-      firstName: {
-        type: Sequelize.STRING,
-
-      },
-      lastName: {
-        type: Sequelize.STRING,
-
-      },
       hashedPassword: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING.BINARY,
         allowNull: false
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     }, options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = "Users";
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable(options);
   }
 };

02bd7eb97b7c0ed44ceff99c8ed9b896c471c355
Your Name <Your Email>
2023-03-20 15:17:06 -0500

    relations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Spot.belongsTo(models.User, { foreignKey: 'ownerId' });
+      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' });
+      Spot.belongsToMany(models.User, { through: models.Booking});
+      Spot.belongsToMany(models.User, { through: models.Review});
     }
   }
   Spot.init({

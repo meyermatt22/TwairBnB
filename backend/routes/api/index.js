5f4ec1c04cd8d8893c9a9673974f159eb3dbca5b
Your Name <Your Email>
2023-03-24 14:44:59 -0500

    get all the current users bookings

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,8 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 const spotsRouter = require('./spots.js');
-const reviewRouter = require('./reviews.js')
+const reviewRouter = require('./reviews.js');
+const bookingRouter = require('./bookings.js');
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -18,6 +19,8 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 
 router.use('/reviews', reviewRouter);
+
+router.use('/bookings', bookingRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });

f85c3dbe27efb8e5683ce1fb31d7abf7c620012a
Your Name <Your Email>
2023-04-09 14:27:02 -0500

    deploy-phase1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,22 +1,8 @@
-
 const express = require('express');
 const router = express.Router();
-
-
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-// router.use('/api', )
-
 
 // Static routes
 // Serve React build files in production
@@ -25,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -35,8 +21,8 @@ if (process.env.NODE_ENV === 'production') {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie('CSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -46,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 

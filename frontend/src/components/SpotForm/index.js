7ea9ee06a3ca63a8e5cd6317b0d4e58cc6ae197c
Your Name <Your Email>
2023-04-13 18:21:15 -0500

    create a spot form

--- frontend/src/components/SpotForm/index.js
+++ frontend/src/components/SpotForm/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom"
 import { createSpot } from "../../store/spots"
-import SpotImageForm from "../SpotImageForm";
+
 
 const SpotForm = ({ spot }) => {
     console.log('SpotForm hit ***')
@@ -13,7 +13,14 @@ const SpotForm = ({ spot }) => {
     const [country, setCountry] = useState(spot?.country)
     const [name, setName] = useState(spot?.name)
     const [description, setDescription] = useState(spot?.description)
+    const [url, setUrl] = useState(spot?.url)
+    const [url1, setUrl1] = useState(spot?.url1)
+    const [url2, setUrl2] = useState(spot?.url2)
+    const [url3, setUrl3] = useState(spot?.url3)
+    const [url4, setUrl4] = useState(spot?.url4)
+    const [preview, setPreview] = useState(true)
     const [price, setPrice] = useState(spot?.price)
+
     const [errors, setErrors] = useState({})
 
     const dispatch = useDispatch();
@@ -22,13 +29,30 @@ const SpotForm = ({ spot }) => {
         console.log('SpotForm handle submit hit ***')
         e.preventDefault();
         setErrors({});
-        spot = { ...spot, address, city, state, country, name, description, price };
+        spot = { ...spot, address, city, state, country, name, description, price, url, url1, url2, url3, url4, preview };
 
+        // const Imgs = {
+        //     url: "https://freepngimg.com/thumb/temple/31704-3-buddha-temple-transparent-background.png",
+        //     preview: true
+        // }
+        const images = [ {url, preview}, {url1, preview}, {url2, preview}, {url3, preview}, {url4, preview} ]
+        // if(images.length) {
+        //     let count = 0
+        //     images.forEach(i => {
+        //         count += 1
+        //         if(count > 1) {
+        //             i.preview = false
+        //         }
+        //         console.log('preview ***',i.preview)
+        //     })
+        // }
+
+        console.log('images : ** ', images)
         // if(formType === "Create Spot") {
-            const newSpot = await dispatch(createSpot(spot))
+            const newSpot = await dispatch(createSpot(spot,images))
             spot = newSpot;
         // }
-        console.log('history update1: ',history)
+        console.log('new spot: ',spot)
         if(spot.errors) {
             setErrors(spot.errors)
         } else {
@@ -119,7 +143,61 @@ const SpotForm = ({ spot }) => {
                 />
                 </label>
             </div>
-            {/* {SpotImageForm(spot)} */}
+
+            <div className="spotImageForm">
+                <h3>Liven up your spot with photos</h3>
+                <p>Submit a link to at least one photo to publish your spot.</p>
+                <div>
+                    <label>
+                     <div className="errors">{errors.url}</div>
+                       <input
+                           type="text"
+                           value={url}
+                           onChange={(e) => setUrl(e.target.value)}
+                       />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                     <div className="errors">{errors.url1}</div>
+                       <input
+                           type="text"
+                           value={url1}
+                           onChange={(e) => setUrl1(e.target.value)}
+                       />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                     <div className="errors">{errors.url2}</div>
+                       <input
+                           type="text"
+                           value={url2}
+                           onChange={(e) => setUrl2(e.target.value)}
+                       />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                     <div className="errors">{errors.url3}</div>
+                       <input
+                           type="text"
+                           value={url3}
+                           onChange={(e) => setUrl3(e.target.value)}
+                       />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                     <div className="errors">{errors.url4}</div>
+                       <input
+                           type="text"
+                           value={url4}
+                           onChange={(e) => setUrl4(e.target.value)}
+                       />
+                    </label>
+                </div>
+            </div>
                 <div className="submitButton">
                  <button type="submit">Create new Spot</button>
                 </div>
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -43,39 +43,48 @@ export const getOneSpot = (spotId) => async (dispatch) => {
     };
 };
 
-export const createSpot = (spot) => async (dispatch) => {
-    const res = await fetch("/api/spots", {
+export const createSpot = (spot, imgs) => async (dispatch) => {
+    let res = await csrfFetch("/api/spots", {
         method: "POST",
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify(spot)
     });
+    const newSpot = await res.json();
 
+    console.log('newspot: ', newSpot)
     if(res.ok) {
-        const newSpot = await res.json();
         dispatch(loadDetails(newSpot));
-        return newSpot;
-    } else {
-        const errors = await res.json()
-        return errors
-    }
+
+        for (let i = 0; i < imgs.length; i++) {
+            res = await csrfFetch(`/api/spots/${newSpot.id}/images`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(imgs[i])
+            });
+        }
+
+            const newSpotImg = await res.json();
+
+            console.log('newspotimg: ', newSpotImg)
+
+            if(res.ok) {
+                dispatch(loadDetails(newSpotImg));
+                return newSpotImg;
+            } else {
+                const errors = await res.json()
+                return errors
+            }
+
+
+     } //else {
+    //     const errors = await res.json()
+    //     return errors
+    // }
+    return newSpot;
 }
 
 export const createSpotImages = (spot) => async (dispatch) => {
     console.log('CREATE SPOT IMAGES HIT ***')
-    const res = await csrfFetch(`/api/spots/${spot}/images`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify(spot)
-    });
-
-    if(res.ok) {
-        const newSpotImg = await res.json();
-        dispatch(loadDetails(newSpotImg));
-        return newSpotImg;
-    } else {
-        const errors = await res.json()
-        return errors
-    }
 }
 
 const initialState = {};
